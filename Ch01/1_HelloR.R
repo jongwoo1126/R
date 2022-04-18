abb6aa5522876655ea628a62b89fa5a72ed135f3
unknown <lucky4527@gmail.com>
2022-04-18 15:45:09 +0900

    2022/04/18 R

---
+++ Ch01/1_HelloR.R
@@ -0,0 +1,15 @@
+# 날짜 : 2022/04/18
+# 이름 : 박종우
+# 내용 : Hello R
+
+print("Hello R!")
+print('안녕 R!')
+
+hello <- "Hello World"
+print(hello)
+
+welcome <- "welcome R!"
+print(welcome)
+
+greeting <- "greeting R"
+print(greeting)
